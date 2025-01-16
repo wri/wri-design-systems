480fb2d692ef08fee9e4bd31d57e6929533596a2
AlejoYarce <alejo.yarce990@gmail.com>
2025-01-15 23:16:13 -0500

    ZeroHeight structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,24 +1,43 @@
-export { default as Switch } from './Forms/Controls/Switch'
+// Forms //
+
+// Buttons
 export { default as Button } from './Forms/Buttons/Button'
-export { default as NavigationRail } from './Navigation/NavigationRail'
+export { default as IconButton } from './Forms/Buttons/IconButton'
+export { default as CloseButton } from './Forms/Buttons/CloseButton'
+
+// Controls
+export { default as Switch } from './Forms/Controls/Switch'
 export { default as Radio } from './Forms/Controls/Radio'
 export { default as RadioGroup } from './Forms/Controls/Radio/RadioGroup'
-export { default as TabBar } from './TabBar'
-export { default as LayerItem } from './Layer/LayerItem'
 export { default as Checkbox } from './Forms/Controls/Checkbox'
+export { default as Slider } from './Forms/Controls/Slider'
+
+// Tags
+export { default as Tag } from './Forms/Tag'
+
+// Inputs
+export { default as TextInput } from './Forms/Inputs/TextInput'
+export { default as Textarea } from './Forms/Inputs/Textarea'
+export { default as Select } from './Forms/Inputs/Select'
+
+// Navigation //
+export { default as TabBar } from './Navigation/TabBar'
+export { default as NavigationRail } from './Navigation/NavigationRail'
+
+// Status //
+export { default as InlineMessage } from './InlineMessage'
+
+// Geospatial //
+
+// Layers
+export { default as LayerItem } from './Layer/LayerItem'
 export { default as LayerGroup } from './Layer/LayerGroup'
 export { default as LayerGroupContainer } from './Layer/LayerGroup/LayerGroupContainer'
 export { default as LayerPanel } from './Layer/LayerPanel'
-export { default as Tag } from './Forms/Tag'
-export { default as InlineMessage } from './InlineMessage'
-export { default as TextInput } from './Forms/Inputs/TextInput'
-export { default as Textarea } from './Forms/Inputs/Textarea'
-export { default as CloseButton } from './Forms/Buttons/CloseButton'
-export { default as IconButton } from './Forms/Buttons/IconButton'
-export { default as Slider } from './Forms/Controls/Slider'
+
+// Legends
 export { default as QualitativeLegend } from './Legend/QualitativeLegend'
 export { default as LayerParameters } from './Legend/LayerParameters'
 export { default as ScaleBar } from './Legend/ScaleBar'
 export { default as LegendItem } from './Legend/LegendItem'
 export { default as LegendPanel } from './Legend/LegendPanel'
-export { default as Select } from './Forms/Inputs/Select'
