cee7077f69c5d399aa87f761b19b660c8049eee2
AlejoYarce <alejo.yarce990@gmail.com>
2025-02-26 21:12:08 -0500

    Button migration

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,57 +1,63 @@
+import { designSystemStyles } from '../lib/theme'
+
+// -- Styles -- //
+export {
+  designSystemStyles,
+}
+
 // -- Forms -- //
 
 // Buttons
 export { default as Button } from './Forms/Buttons/Button'
-export { default as CloseButton } from './Forms/Buttons/CloseButton'
-export { default as IconButton } from './Forms/Buttons/IconButton'
-export { default as MultiActionButton } from './Forms/Buttons/MultiActionButton'
+// export { default as CloseButton } from './Forms/Buttons/CloseButton'
+// export { default as IconButton } from './Forms/Buttons/IconButton'
+// export { default as MultiActionButton } from './Forms/Buttons/MultiActionButton'
 
-// Controls
-export { default as Checkbox } from './Forms/Controls/Checkbox'
-export {
-  default as OptionCard,
-  OptionCardGroup,
-} from './Forms/Controls/OptionCard'
-export { default as Radio } from './Forms/Controls/Radio'
-export { default as RadioGroup } from './Forms/Controls/Radio/RadioGroup'
-export { default as Slider } from './Forms/Controls/Slider'
-export { default as Switch } from './Forms/Controls/Switch'
+// // Controls
+// export { default as Checkbox } from './Forms/Controls/Checkbox'
+// export { default as OptionCard } from './Forms/Controls/OptionCard'
+// export { default as OptionCardGroup } from './Forms/Controls/OptionCard/OptionCardGroup'
+// export { default as Radio } from './Forms/Controls/Radio'
+// export { default as RadioGroup } from './Forms/Controls/Radio/RadioGroup'
+// export { default as Slider } from './Forms/Controls/Slider'
+// export { default as Switch } from './Forms/Controls/Switch'
 
-// Inputs
-export { default as CheckboxList } from './Forms/Inputs/CheckboxList'
-export { default as InputWithUnits } from './Forms/Inputs/InputWithUnits'
-export { default as Password } from './Forms/Inputs/Password'
-export { default as RadioList } from './Forms/Inputs/RadioList'
-export { default as Select } from './Forms/Inputs/Select'
-export { default as SliderInput } from './Forms/Inputs/SliderInput'
-export { default as Textarea } from './Forms/Inputs/Textarea'
-export { default as TextInput } from './Forms/Inputs/TextInput'
+// // Inputs
+// export { default as CheckboxList } from './Forms/Inputs/CheckboxList'
+// export { default as InputWithUnits } from './Forms/Inputs/InputWithUnits'
+// export { default as Password } from './Forms/Inputs/Password'
+// export { default as RadioList } from './Forms/Inputs/RadioList'
+// export { default as Select } from './Forms/Inputs/Select'
+// export { default as SliderInput } from './Forms/Inputs/SliderInput'
+// export { default as Textarea } from './Forms/Inputs/Textarea'
+// export { default as TextInput } from './Forms/Inputs/TextInput'
 
-// Tags
-export { default as Tag } from './Forms/Tag'
+// // Tags
+// export { default as Tag } from './Forms/Tag'
 
-// -- Geospatial -- //
+// // -- Geospatial -- //
 
-// Layers
-export { default as LayerGroup } from './Geospatial/Layers/LayerGroup'
-export { default as LayerGroupContainer } from './Geospatial/Layers/LayerGroup/LayerGroupContainer'
-export { default as LayerItem } from './Geospatial/Layers/LayerItem'
-export { default as LayerPanel } from './Geospatial/Layers/LayerPanel'
+// // Layers
+// export { default as LayerGroup } from './Geospatial/Layers/LayerGroup'
+// export { default as LayerGroupContainer } from './Geospatial/Layers/LayerGroup/LayerGroupContainer'
+// export { default as LayerItem } from './Geospatial/Layers/LayerItem'
+// export { default as LayerPanel } from './Geospatial/Layers/LayerPanel'
 
-// Legends
-export { default as LayerParameters } from './Geospatial/Legends/LayerParameters'
-export { default as LegendItem } from './Geospatial/Legends/LegendItem'
-export { default as LegendPanel } from './Geospatial/Legends/LegendPanel'
-export { default as QualitativeAttribute } from './Geospatial/Legends/QualitativeAttribute'
-export { default as ScaleBar } from './Geospatial/Legends/ScaleBar'
+// // Legends
+// export { default as LayerParameters } from './Geospatial/Legends/LayerParameters'
+// export { default as LegendItem } from './Geospatial/Legends/LegendItem'
+// export { default as LegendPanel } from './Geospatial/Legends/LegendPanel'
+// export { default as QualitativeAttribute } from './Geospatial/Legends/QualitativeAttribute'
+// export { default as ScaleBar } from './Geospatial/Legends/ScaleBar'
 
-// -- Navigation -- //
-export { default as Breadcrumb } from './Navigation/Breadcrumb'
-export { default as Footer } from './Navigation/Footer'
-export { default as NavigationRail } from './Navigation/NavigationRail'
-export { default as TabBar } from './Navigation/TabBar'
+// // -- Navigation -- //
+// export { default as Breadcrumb } from './Navigation/Breadcrumb'
+// export { default as Footer } from './Navigation/Footer'
+// export { default as NavigationRail } from './Navigation/NavigationRail'
+// export { default as TabBar } from './Navigation/TabBar'
 
-// -- Status -- //
-export { default as Badge } from './Status/Badge'
-export { default as InlineMessage } from './Status/InlineMessage'
-export { default as Toast, showToast } from './Status/Toast'
+// // -- Status -- //
+// export { default as Badge } from './Status/Badge'
+// export { default as InlineMessage } from './Status/InlineMessage'
+// export { default as Toast } from './Status/Toast'
+// export { showToast } from './Status/Toast/utils'
