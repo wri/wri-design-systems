33eaf83cc133cce78222d9137daa70d0f4479ac1
AlejoYarce <alejo.yarce990@gmail.com>
2025-03-02 10:44:23 -0500

    TextInput migration

--- src/components/Forms/Inputs/TextInput/styled.ts
+++ src/components/Forms/Inputs/TextInput/styled.ts
@@ -1,19 +1,19 @@
-import styled from '@emotion/styled'
-import { Input, Field } from '@chakra-ui/react'
+import { css } from '@emotion/react'
 import { getThemedColor } from '../../../../lib/theme'
+import { TextInputProps } from './types'
 
-export const TextInputContainer = styled.div<{ size: 'small' | 'default' }>`
+export const textInputContainerStyles = (size: TextInputProps['size']) => css`
   position: relative;
   height: 100%;
   width: 100%;
   display: flex;
   justify-content: flex-start;
   align-items: flex-start;
-  gap: ${({ size }) => (size === 'small' ? '12px' : '16px')};
+  gap: ${size === 'small' ? '12px' : '16px'};
   margin-bottom: 20px;
 `
 
-export const ErrorBar = styled.div`
+export const textInputErrorBarStyles = css`
   width: 3px;
   height: 100%;
   background-color: ${getThemedColor('error', 500)};
@@ -22,51 +22,54 @@ export const ErrorBar = styled.div`
   left: 0;
 `
 
-export const StyledFieldLabel = styled(Field.Label)<{
-  size: 'small' | 'default'
-  disabled?: boolean
-}>`
-  color: ${({ disabled }) => getThemedColor('neutral', disabled ? 600 : 900)};
-  font-size: ${({ size }) => (size === 'small' ? '14px' : '16px')};
+export const fieldLabelStyles = (
+  size: TextInputProps['size'],
+  disabled?: TextInputProps['disabled'],
+) => css`
+  color: ${getThemedColor('neutral', disabled ? 600 : 900)};
+  font-size: ${size === 'small' ? '14px' : '16px'};
   font-weight: 400;
-  line-height: ${({ size }) => (size === 'small' ? '20px' : '24px')};
+  line-height: ${size === 'small' ? '20px' : '24px'};
   margin-bottom: 2px;
 
   span {
-    color: ${({ disabled }) => getThemedColor('neutral', disabled ? 600 : 700)};
+    color: ${getThemedColor('neutral', disabled ? 600 : 700)};
   }
 
   .chakra-field__requiredIndicator {
-    color: ${({ disabled }) =>
-      disabled ? getThemedColor('neutral', 600) : getThemedColor('error', 500)};
+    color: ${disabled
+      ? getThemedColor('neutral', 600)
+      : getThemedColor('error', 500)};
   }
 `
 
-export const StyledFieldCaption = styled(Field.HelperText)<{
-  size: 'small' | 'default'
-  disabled?: boolean
-}>`
-  color: ${({ disabled }) => getThemedColor('neutral', disabled ? 500 : 600)};
-  font-size: ${({ size }) => (size === 'small' ? '12px' : '14px')};
+export const fieldCaptionStyles = (
+  size: TextInputProps['size'],
+  disabled?: TextInputProps['disabled'],
+) => css`
+  color: ${getThemedColor('neutral', disabled ? 500 : 600)};
+  font-size: ${size === 'small' ? '12px' : '14px'};
   font-weight: 400;
-  line-height: ${({ size }) => (size === 'small' ? '16px' : '20px')};
+  line-height: ${size === 'small' ? '16px' : '20px'};
 `
 
-export const StyledFieldErrorMessage = styled(Field.ErrorText)<{
-  size: 'small' | 'default'
-}>`
+export const fieldErrorMessageStyles = (size: TextInputProps['size']) => css`
   color: ${getThemedColor('error', 500)};
-  font-size: ${({ size }) => (size === 'small' ? '12px' : '14px')};
+  font-size: ${size === 'small' ? '12px' : '14px'};
   font-weight: 700;
-  line-height: ${({ size }) => (size === 'small' ? '16px' : '20px')};
+  line-height: ${size === 'small' ? '16px' : '20px'};
   margin-top: 2px;
 `
 
-export const StyledTextInput = styled(Input)<{ size: 'small' | 'default' }>`
-  height: ${({ size }) => (size === 'small' ? '28px' : '40px')};
+export const textInputStyles = (
+  size: TextInputProps['size'],
+  value?: string,
+  defaultValue?: string,
+) => css`
+  height: ${size === 'small' ? '28px' : '40px'};
   width: 100%;
   border-radius: 4px;
-  padding: ${({ size }) => (size === 'small' ? '4px 8px' : '12px')};
+  padding: ${size === 'small' ? '4px 8px' : '12px'};
   margin-top: 8px;
   background-color: ${getThemedColor('neutral', 100)};
   color: ${getThemedColor('neutral', 800)};
@@ -95,10 +98,9 @@ export const StyledTextInput = styled(Input)<{ size: 'small' | 'default' }>`
     border: 1px solid ${getThemedColor('neutral', 300)} !important;
   }
 
-  ${({ value, defaultValue }) =>
-    value || defaultValue
-      ? `
+  ${value || defaultValue
+    ? `
     border: 1px solid ${getThemedColor('neutral', 700)};
   `
-      : ''}
+    : ''}
 `
