fc0cb9d0d9a0aabc7fe60e911cf01deb5bb71399
AlejoYarce <alejo.yarce990@gmail.com>
2025-03-06 10:54:56 -0500

    LayerSidebar migration

--- src/components/Forms/Controls/Radio/types.ts
+++ src/components/Forms/Controls/Radio/types.ts
@@ -10,11 +10,12 @@ export type RadioProps = Omit<
 
 export type RadioGroupProps = Omit<
   ChakraRadioGroup.ItemProps,
-  'size' | 'variant' | 'colorPalette' | 'value' | 'onChange'
+  'size' | 'variant' | 'colorPalette' | 'value' | 'onChange' | 'children'
 > & {
   name: string
   defaultValue?: string
   horizontal?: boolean
   onChange?: (name: string, selectedValue: string) => void
   customGap?: string
+  children?: React.ReactNode
 }
