5a873f53d1764edbe1474fd84927bf8e888c80e9
AlejoYarce <alejo.yarce990@gmail.com>
2025-04-02 21:24:40 -0500

    Color palette updates

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -7,8 +7,9 @@ export type ThemeProps = {
     | 'secondary'
     | 'success'
     | 'warning'
-    | 'error']: {
-    [index: number]: {
+    | 'error'
+    | 'accessible']: {
+    [index: number | string]: {
       value: string
     }
   }
@@ -20,41 +21,54 @@ export const wriTheme: ThemeProps = {
     200: { value: '#F6F6F6' },
     300: { value: '#E7E6E6' },
     400: { value: '#C9C9C9' },
-    500: { value: '#9B9B9B' },
-    600: { value: '#686767' },
-    700: { value: '#4F4E4E' },
-    800: { value: '#1A1919' },
-    900: { value: '#000000' },
+    500: { value: '#B0B0B0' },
+    600: { value: '#A1A1A1' },
+    700: { value: '#5C5959' },
+    800: { value: '#3D3B3B' },
+    900: { value: '#1A1919' },
   },
   primary: {
     100: { value: '#FFFBF2' },
     200: { value: '#FCEFD3' },
     500: { value: '#F0AB00' },
-    600: { value: '#D49700' },
-    700: { value: '#AD7500' },
+    600: { value: '#DE9E00' },
+    700: { value: '#855B00' },
     800: { value: '#5F4205' },
     900: { value: '#332300' },
   },
   secondary: {
+    300: { value: '#B2C3F0' },
+    400: { value: '#4F6CBA' },
     500: { value: '#3855A3' },
-    600: { value: '#263A71' },
-    700: { value: '#1F2F5C' },
+    600: { value: '#0A4298' },
+    700: { value: '#123369' },
   },
   success: {
-    100: { value: '#F3FFF2' },
+    100: { value: '#EBF5F2' },
     200: { value: '#D3EED1' },
-    300: { value: '#C7E9C0' },
-    500: { value: '#086600' },
+    300: { value: '#C2E5DC' },
+    500: { value: '#009E77' },
+    900: { value: '#00664D' },
   },
   warning: {
-    100: { value: '#FEF7E5' },
+    100: { value: '#FBF7EA' },
     200: { value: '#E3CC8F' },
-    500: { value: '#804600' },
+    300: { value: '#EEDDA5' },
+    500: { value: '#A88100' },
+    900: { value: '#715804' },
   },
   error: {
-    100: { value: '#FFF3F4' },
+    100: { value: '#FFEFED' },
     200: { value: '#EDA1A9' },
-    500: { value: '#A80011' },
+    300: { value: '#F6C5C1' },
+    500: { value: '#C11101' },
+    900: { value: '#8D0D01' },
+  },
+  accessible: {
+    'text-on-primary-mids': { value: '#332300' }, // primary 900
+    'text-on-secondary-mids': { value: '#F2F6FF' }, // secondary 100
+    'controls-on-neutral-lights': { value: '#855B00' }, // primary 700
+    'controls-on-neutral-darks': { value: '#F5BF4F' }, // primary 400
   },
 }
 
@@ -73,8 +87,22 @@ export const getThemedColor = (
     | 'secondary'
     | 'success'
     | 'warning'
-    | 'error',
-  index: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900,
+    | 'error'
+    | 'accessible',
+  index:
+    | 100
+    | 200
+    | 300
+    | 400
+    | 500
+    | 600
+    | 700
+    | 800
+    | 900
+    | 'text-on-primary-mids'
+    | 'text-on-secondary-mids'
+    | 'controls-on-neutral-lights'
+    | 'controls-on-neutral-darks',
 ): string =>
   designSystemStyles.tokens.getVar(`colors.${variant}.${index}`) ||
   wriTheme?.[variant]?.[index]?.value
