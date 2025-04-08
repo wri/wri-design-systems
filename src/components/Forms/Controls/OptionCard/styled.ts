dc4450e39c4948390274d5fe8644fe47febcb90e
AlejoYarce <alejo.yarce990@gmail.com>
2025-04-08 10:43:55 -0500

    OptionCard - double focus border

--- src/components/Forms/Controls/OptionCard/styled.ts
+++ src/components/Forms/Controls/OptionCard/styled.ts
@@ -24,6 +24,7 @@ export const optionCardContainerStyles = css`
     outline-offset: 2px;
     box-shadow: 0px 2px 4px -2px #0000001a;
     box-shadow: 0px 4px 6px -1px #0000001a;
+    box-shadow: 0 0 0 2px ${getThemedColor('neutral', 100)}, rgba(0, 0, 0, 0.05) 0px 2px 2px 4px;
   }
 
   &[data-state='checked'] {
