e44807ddc8b382edcfd7d6c5696333988f612e79
AlejoYarce <alejo.yarce990@gmail.com>
2025-04-08 11:27:15 -0500

    Textarea - double focus border

--- src/components/Forms/Inputs/Textarea/styled.ts
+++ src/components/Forms/Inputs/Textarea/styled.ts
@@ -88,6 +88,7 @@ export const textareaSyles = (
     border: 2px solid ${getThemedColor('neutral', 700)};
     outline: 2px solid ${getThemedColor('primary', 700)};
     outline-offset: 2px;
+    box-shadow: 0 0 0 2px ${getThemedColor('neutral', 100)}, rgba(0, 0, 0, 0.05) 0px 2px 2px 4px;
   }
 
   &:active {
