382b068702861beed3afb5b97bdc664182b5e0f5
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2025-10-14 18:05:34 +0400

    adds List component demo to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ import ScaleBarDemo from './components/Geospatial/Legends/ScaleBar/ScaleBarDemo'
 
 import ItemCountDemo from './components/DataDisplay/ItemCount/ItemCountDemo'
 import TableDemo from './components/DataDisplay/Table/TableDemo'
+import ListDemo from './components/DataDisplay/List/ListDemo'
 
 import ModalDemo from './components/Containers/Modal/ModalDemo'
 import PanelDemo from './components/Containers/Panel/PanelDemo'
@@ -123,6 +124,7 @@ const App = () => (
         <ItemCountDemo />
         <PaginationDemo />
         <TableDemo />
+        <ListDemo />
 
         <PanelDemo />
         <SheetDemo />
