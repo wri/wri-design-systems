7838e5e4f2a2946bfc599dcf3efb0c91a4d2a61c
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2025-10-16 18:17:07 +0400

    DS-200-lists_component_fixes

--- src/components/DataDisplay/List/styled.ts
+++ src/components/DataDisplay/List/styled.ts
@@ -61,8 +61,6 @@ export const listItemNavigationStyles = css`
   &:hover {
     box-shadow: 0px 2px 4px -2px #0000001a;
     box-shadow: 0px 4px 6px -1px #0000001a;
-  }
-  &:hover {
     background-color: color-mix(
       in srgb,
       ${getThemedColor('primary', 500)} 20%,
