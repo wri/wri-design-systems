0fc4cd9e96b8abd976eb1c3c816e14a3864c8dde
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-01-08 11:33:12 +0200

    DS-77-remove_chart_component (#107)
    Co-authored-by: José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>

--- src/components/DataDisplay/Charts/ChartContainer/types.ts
+++
@@ -1,9 +0,0 @@
-import { type BoxProps } from '@chakra-ui/react'
-
-export interface ChartContainerProps extends BoxProps {
-  isLoading?: boolean
-  isError?: boolean
-  isEmpty?: boolean
-  errorMessage?: string
-  children: React.ReactNode
-}
