d4f91196bc41440c93387f6a73040b3fae7f6e4b
AlejoYarce <alejo.yarce990@gmail.com>
2026-01-28 12:45:08 -0500

    Refactor Radio components to use controlled value prop instead of defaultValue; update README and stories accordingly

--- src/components/Forms/Controls/Radio/Radio.stories.tsx
+++ src/components/Forms/Controls/Radio/Radio.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 
 import type { Meta, StoryObj } from '@storybook/react'
 import Radio from '.'
@@ -21,41 +21,65 @@ type Story = StoryObj<typeof meta>
 export const SingleRadio: Story = {
   args: {
     name: 'single-radio',
-    onChange: (name, selectedValue) => console.log(name, selectedValue),
+    value: '1',
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value='1'>One</Radio>
-    </RadioGroup>
-  ),
+  render: (args) => {
+    const [value, setValue] = useState(args.value)
+
+    return (
+      <RadioGroup
+        {...args}
+        value={value}
+        onChange={(name, selectedValue) => setValue(selectedValue)}
+      >
+        <Radio value='1'>One</Radio>
+      </RadioGroup>
+    )
+  },
 }
 
 export const DefaultChecked: Story = {
   args: {
     name: 'default-checked',
-    defaultValue: '2',
+    value: '2',
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value='1'>One</Radio>
-      <Radio value='2'>Two</Radio>
-      <Radio value='3'>Three</Radio>
-    </RadioGroup>
-  ),
+  render: (args) => {
+    const [value, setValue] = useState(args.value)
+
+    return (
+      <RadioGroup
+        {...args}
+        value={value}
+        onChange={(name, selectedValue) => setValue(selectedValue)}
+      >
+        <Radio value='1'>One</Radio>
+        <Radio value='2'>Two</Radio>
+        <Radio value='3'>Three</Radio>
+      </RadioGroup>
+    )
+  },
 }
 
 export const Disabled: Story = {
   args: {
     name: 'default-checked',
-    defaultValue: '2',
+    value: '2',
   },
-  render: (args) => (
-    <RadioGroup {...args}>
-      <Radio value='1'>One</Radio>
-      <Radio value='2' disabled>
-        Two
-      </Radio>
-      <Radio value='3'>Three</Radio>
-    </RadioGroup>
-  ),
+  render: (args) => {
+    const [value, setValue] = useState(args.value)
+
+    return (
+      <RadioGroup
+        {...args}
+        value={value}
+        onChange={(name, selectedValue) => setValue(selectedValue)}
+      >
+        <Radio value='1'>One</Radio>
+        <Radio value='2' disabled>
+          Two
+        </Radio>
+        <Radio value='3'>Three</Radio>
+      </RadioGroup>
+    )
+  },
 }
