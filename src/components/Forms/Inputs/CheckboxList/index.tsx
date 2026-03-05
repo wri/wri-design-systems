759e772e56fb8210693572ece55568d5ea8ed130
AlejoYarce <alejo.yarce990@gmail.com>
2026-03-05 14:46:51 -0500

    [CheckboxList] - fixing external control (#127)
    * [CheckboxList] - fixing external control
    
    * lint fix

--- src/components/Forms/Inputs/CheckboxList/index.tsx
+++ src/components/Forms/Inputs/CheckboxList/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 /** @jsxImportSource @emotion/react */
 
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Group } from '@chakra-ui/react'
 import { CheckboxListProps } from './types'
 import Checkbox from '../../Controls/Checkbox'
@@ -33,16 +33,58 @@ const CheckboxList = ({
   const normalizedDefaultValues = Array.isArray(defaultValues)
     ? defaultValues
     : []
+  const normalizedDefaultValueSet = useMemo(
+    () => new Set(normalizedDefaultValues),
+    [normalizedDefaultValues],
+  )
+
+  const buildInitialCheckedValues = () =>
+    checkboxes
+      .filter((checkbox) => checkbox.name)
+      .reduce(
+        (accumulator, checkbox) => {
+          const name = checkbox.name as string
+          accumulator[name] =
+            typeof checkbox.checked === 'boolean'
+              ? checkbox.checked
+              : normalizedDefaultValueSet.has(name)
+          return accumulator
+        },
+        {} as Record<string, boolean>,
+      )
 
   const [checkedValues, setCheckedValues] = useState<{
     [name: string]: boolean
-  }>(
-    normalizedDefaultValues.reduce(
-      (acc, name) => ({ ...acc, [name]: true }),
-      {},
-    ),
-  )
+  }>(buildInitialCheckedValues)
   const [isExpanded, setIsExpanded] = useState(true)
+
+  useEffect(() => {
+    setCheckedValues((previousCheckedValues) => {
+      const nextCheckedValues = checkboxes
+        .filter((checkbox) => checkbox.name)
+        .reduce(
+          (accumulator, checkbox) => {
+            const name = checkbox.name as string
+            accumulator[name] =
+              typeof checkbox.checked === 'boolean'
+                ? checkbox.checked
+                : (previousCheckedValues[name] ??
+                  normalizedDefaultValueSet.has(name))
+            return accumulator
+          },
+          {} as Record<string, boolean>,
+        )
+
+      const hasSameKeysAndValues =
+        Object.keys(previousCheckedValues).length ===
+          Object.keys(nextCheckedValues).length &&
+        Object.entries(nextCheckedValues).every(
+          ([name, value]) => previousCheckedValues[name] === value,
+        )
+
+      return hasSameKeysAndValues ? previousCheckedValues : nextCheckedValues
+    })
+  }, [checkboxes, normalizedDefaultValueSet])
 
   const handleOnChecked = (checked: string | boolean, name?: string) => {
     const newCheckedValues = { ...checkedValues }
@@ -56,15 +98,28 @@ const CheckboxList = ({
     }
   }
   const hasParentCheckbox = typeof label !== 'string'
+  const resolvedCheckedValues = checkboxes
+    .filter((checkbox) => checkbox.name)
+    .reduce(
+      (accumulator, checkbox) => {
+        const name = checkbox.name as string
+        accumulator[name] =
+          typeof checkbox.checked === 'boolean'
+            ? checkbox.checked
+            : !!checkedValues[name]
+        return accumulator
+      },
+      {} as Record<string, boolean>,
+    )
 
   const allChecked =
     checkboxes.length > 0 &&
     checkboxes.every(
-      (checkbox) => checkbox.name && checkedValues[checkbox.name],
+      (checkbox) => checkbox.name && resolvedCheckedValues[checkbox.name],
     )
 
   const someChecked = checkboxes.some(
-    (checkbox) => checkbox.name && checkedValues[checkbox.name],
+    (checkbox) => checkbox.name && resolvedCheckedValues[checkbox.name],
   )
 
   const indeterminate = someChecked && !allChecked
@@ -87,7 +142,7 @@ const CheckboxList = ({
   const totalCount = checkboxes.length
 
   const checkedCount = checkboxes.filter(
-    (checkbox) => checkbox?.name && checkedValues[checkbox.name],
+    (checkbox) => checkbox?.name && resolvedCheckedValues[checkbox.name],
   ).length
   const counterText = `${checkedCount} / ${totalCount}`
 
@@ -167,18 +222,28 @@ const CheckboxList = ({
           id='checkbox-list'
           css={checkboxListContentListStyles(horizontal, isExpanded)}
         >
-          {checkboxes.map((checkbox) => (
-            <Checkbox
-              ms={hasParentCheckbox && !horizontal ? '6' : ''}
-              key={checkbox.name}
-              css={{}}
-              checked={checkbox?.name ? !!checkedValues[checkbox.name] : false}
-              onCheckedChange={({ checked }) =>
-                handleOnChecked(checked, checkbox.name)
-              }
-              {...checkbox}
-            />
-          ))}
+          {checkboxes.map((checkbox) => {
+            const { onCheckedChange: itemOnCheckedChange, ...checkboxProps } =
+              checkbox
+
+            return (
+              <Checkbox
+                ms={hasParentCheckbox && !horizontal ? '6' : ''}
+                key={checkbox.name}
+                css={{}}
+                checked={
+                  checkbox?.name
+                    ? !!resolvedCheckedValues[checkbox.name]
+                    : false
+                }
+                onCheckedChange={({ checked }) => {
+                  handleOnChecked(checked, checkbox.name)
+                  itemOnCheckedChange?.({ checked: !!checked })
+                }}
+                {...checkboxProps}
+              />
+            )
+          })}
         </div>
       </div>
     </Group>
