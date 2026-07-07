66b4a08041dd1b1adc3fc1f47adc26c83829ee82
Carlos Pesina <pesinasiller@users.noreply.github.com>
2026-07-07 13:34:22 +0300

    menu_link_fix

--- src/components/Forms/Actions/Menu/MenuItem.tsx
+++ src/components/Forms/Actions/Menu/MenuItem.tsx
@@ -12,6 +12,51 @@ import {
   menuItemLabelContentStyles,
 } from './menuItemStyled'
 
+const MenuItemContent = ({
+  item,
+  isChecked,
+  labels,
+}: {
+  item: MenuItemProps
+  isChecked: boolean
+  labels: MenuLabels
+}) => (
+  <>
+    {item.startIcon}
+    <div css={menuItemLabelAndCaptionStyles(!!item.startIcon, !!item.endIcon)}>
+      <div css={menuItemLabelContentStyles(isChecked)}>
+        <p className='ds-menu-item-label'>{item.label}</p>
+        {item.command ? (
+          <ChakraMenu.ItemCommand
+            aria-label={`${labels.shortcutPrefix} ${item.command}`}
+          >
+            {item.command}
+          </ChakraMenu.ItemCommand>
+        ) : null}
+      </div>
+      {item.caption ? (
+        <p className='ds-menu-item-caption'>{item.caption}</p>
+      ) : null}
+    </div>
+    {isChecked ? <CheckIcon /> : item.endIcon}
+  </>
+)
+
+const renderMenuItemBody = (
+  item: MenuItemProps,
+  isChecked: boolean,
+  labels: MenuLabels,
+) => {
+  if (item.children) return item.children
+  if (item.link)
+    return (
+      <a href={item.link} target='_blank' rel='noopener noreferrer'>
+        <MenuItemContent item={item} isChecked={isChecked} labels={labels} />
+      </a>
+    )
+  return <MenuItemContent item={item} isChecked={isChecked} labels={labels} />
+}
+
 const MenuItem = ({
   item,
   isChecked = false,
@@ -27,32 +72,9 @@ const MenuItem = ({
     disabled={item.disabled}
     role='menuitem'
     aria-label={item.label}
+    asChild={!!item.link}
   >
-    {item.children ? (
-      item.children
-    ) : (
-      <>
-        {item.startIcon}
-        <div
-          css={menuItemLabelAndCaptionStyles(!!item.startIcon, !!item.endIcon)}
-        >
-          <div css={menuItemLabelContentStyles(isChecked)}>
-            <p className='ds-menu-item-label'>{item.label}</p>
-            {item.command ? (
-              <ChakraMenu.ItemCommand
-                aria-label={`${labels.shortcutPrefix} ${item.command}`}
-              >
-                {item.command}
-              </ChakraMenu.ItemCommand>
-            ) : null}
-          </div>
-          {item.caption ? (
-            <p className='ds-menu-item-caption'>{item.caption}</p>
-          ) : null}
-        </div>
-        {isChecked ? <CheckIcon /> : item.endIcon}
-      </>
-    )}
+    {renderMenuItemBody(item, isChecked, labels)}
   </ChakraMenu.Item>
 )
 
